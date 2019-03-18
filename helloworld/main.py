437f220987727357799f6a0d9887df9ce7cc56d8
Ewnetu Bayuh Lakew <ewnetu@cs.umu.se>
2019-03-18 15:42:47 +0100

    Update main.py

--- helloworld/main.py
+++ helloworld/main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
  
 @app.route("/")
 def hello():
-return "Hello World!"
+  return "Hello World!"
  
 if __name__ == "__main__":
-app.run(debug=True,host='0.0.0.0')
+  app.run(debug=True,host='0.0.0.0')
